fff223b156a95416f5ddda8a3409844ce1f2f009
Beriac Mathieu <mathieu.beriac@ynov.com>
2024-11-19 17:04:34 +0100

    feat(server): add style.css

--- internal/server.go
+++ internal/server.go
@@ -2,13 +2,38 @@ package internal
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
 const PortNum string = ":3000"
 
+func Init_Server() {
+	router := http.NewServeMux()
+	log.Println("Starting our simple http server.")
+
+	RootHandler(router)
+	log.Println("Started on port", PortNum)
+	fmt.Println("To close connection CRLT+C")
+
+	fs := http.FileServer(http.Dir("front-end"))
+	router.Handle("/front-end/", http.StripPrefix("/front-end/", fs))
+
+	err := http.ListenAndServe(PortNum, router)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Homepage")
+	t := template.Must(template.ParseGlob("./front-end/*.gohtml"))
+	err := t.Execute(w, nil)
+	if err != nil {
+		return
+	}
+
 }
 
 func Info(w http.ResponseWriter, r *http.Request) {
