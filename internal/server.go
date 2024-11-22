897829797d3740e2b52169d7625a25bc195b57af
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2024-11-22 14:41:38 +0100

    fix(index.html, style.css): fixing pictures error

--- internal/server.go
+++ internal/server.go
@@ -28,7 +28,7 @@ func Init_Server() {
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	t := template.Must(template.ParseGlob("./front-end/*.gohtml"))
+	t := template.Must(template.ParseGlob("./front-end/index.gohtml"))
 	err := t.Execute(w, nil)
 	if err != nil {
 		return
