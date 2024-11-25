555692b3f75f386181aa8c37aae86f185de21e7f
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2024-11-25 22:35:28 +0100

    fix(struct): HangManData struc has been modified

---
+++ internal/hangman-classic/internal-hangman-classic/input/input.go
@@ -0,0 +1,45 @@
+package input
+
+import (
+	"net/http"
+	"unicode"
+)
+
+/*
+	 func Input() string {
+		var userInput string
+		// all the forbidden characters are written in the following string
+		forbiddenCharacters := "1234567890°+&é\"(-è_çà)'=^$*ù!:;,?.§%¨£µ€<>~#{[|`\\]}@"
+		fmt.Println("Enter an input :")
+		data, err := fmt.Scanf("%s \n", &userInput)
+		// handling errors
+		if err != nil || data != 1 {
+			fmt.Println("Error")
+		}
+		for i := 0; i < len(userInput); i++ {
+			for j := 0; j < len(forbiddenCharacters); j++ {
+				if userInput[i] == forbiddenCharacters[j] {
+					fmt.Println("Error")
+				}
+			}
+		}
+		fmt.Println("You've chosen : " + userInput)
+		return userInput
+	}
+*/
+func IsLetter(s string) bool {
+	for _, r := range s {
+		if !unicode.IsLetter(r) {
+			return false
+		}
+	}
+	return true
+}
+
+func Input(r *http.Request) string {
+	guess := r.URL.Query().Get("guess")
+	if IsLetter(guess) == true {
+		return guess
+	}
+	return ""
+}
---
+++ internal/hangman-classic/pkg/structs/structs.go
@@ -0,0 +1,10 @@
+package structs
+
+type HangManData struct {
+	Word         string   `json:"word"`     // Word composed of '_', ex: H_ll_
+	ToFind       string   `json:"toFind"`   // Final word chosen by the program at the beginning. It is the word to find
+	Attempts     int      `json:"attempts"` // Number of attempts left
+	WordFile     string   `json:"wordFile"`
+	AlreadyTried []string `json:"alreadyTried"`
+	Nickname     string   `json:"nickname"`
+}
