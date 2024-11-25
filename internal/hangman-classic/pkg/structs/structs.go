ddcb88744b4c990baf5a83064835df711669e278
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2024-11-26 00:58:58 +0100

    fix(struct): StrAttempts has been added to being used on the call for the hangman image

--- internal/hangman-classic/pkg/structs/structs.go
+++ internal/hangman-classic/pkg/structs/structs.go
@@ -4,6 +4,7 @@ type HangManData struct {
 	Word         string   `json:"word"`     // Word composed of '_', ex: H_ll_
 	ToFind       string   `json:"toFind"`   // Final word chosen by the program at the beginning. It is the word to find
 	Attempts     int      `json:"attempts"` // Number of attempts left
+	StrAttempts  string   `json:"strAttempts"`
 	WordFile     string   `json:"wordFile"`
 	AlreadyTried []string `json:"alreadyTried"`
 	Nickname     string   `json:"nickname"`
