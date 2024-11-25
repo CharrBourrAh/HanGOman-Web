68213127f4f7843e40c255fa0e211535d212b77d
Beriac Mathieu <mathieu.beriac@ynov.com>
2024-11-25 13:40:27 +0100

    test(all): if all files are working as intended

--- internal/route.go
+++ internal/route.go
@@ -1,10 +1,20 @@
 package internal
 
 import (
+	"html/template"
 	"net/http"
 )
+
+// var game GameState
+var tmpl *template.Template
+
+func Init() {
+	tmpl = template.Must(template.ParseGlob("templates/*.html"))
+}
 
 func RootHandler(server *http.ServeMux) {
 	server.HandleFunc("/", Home)
 	server.HandleFunc("/how-to-play", HowToPlay)
+	//server.HandleFunc("/guess", handleGuess)
+	//server.HandleFunc("newgame", handleNewGame)
 }
