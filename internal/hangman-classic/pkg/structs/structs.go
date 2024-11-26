4a833cdc1b91e30f9a11f33fc90525ebf67294f3
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2024-11-26 23:50:03 +0100

    fix(struct): StrAttempts has been removed (useless)

--- internal/hangman-classic/pkg/structs/structs.go
+++ internal/hangman-classic/pkg/structs/structs.go
@@ -4,7 +4,6 @@ type HangManData struct {
 	Word         string   `json:"word"`     // Word composed of '_', ex: H_ll_
 	ToFind       string   `json:"toFind"`   // Final word chosen by the program at the beginning. It is the word to find
 	Attempts     int      `json:"attempts"` // Number of attempts left
-	StrAttempts  string   `json:"strAttempts"`
 	WordFile     string   `json:"wordFile"`
 	AlreadyTried []string `json:"alreadyTried"`
 	Nickname     string   `json:"nickname"`
