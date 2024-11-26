b75f6bfa8404a3a6d01f596e4f22d6022f28fec6
Beriac Mathieu <mathieu.beriac@ynov.com>
2024-11-26 17:19:00 +0100

    test(leaderboard): modif of save and server

--- internal/route.go
+++ internal/route.go
@@ -8,4 +8,6 @@ func RootHandler(server *http.ServeMux) {
 	server.HandleFunc("/", Home)
 	server.HandleFunc("/how-to-play", HowToPlay)
 	server.HandleFunc("/game", Game)
+	server.HandleFunc("/leaderboard", LeaderBoard)
+
 }
--- internal/server.go
+++ internal/server.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"html/template"
 	"log"
+	"main/internal/hangman-classic/pkg/structs"
 	"net/http"
 )
 
 const PortNum string = ":3000"
 
 func Init_Server() {
+
 	router := http.NewServeMux()
 	log.Println("Starting our simple http server.")
 
@@ -50,4 +52,29 @@ func Game(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		return
 	}
+}
+
+func LeaderBoard(w http.ResponseWriter, r *http.Request) {
+	var LeaderboardList []structs.Score
+	LeaderboardList = []structs.Score{
+		{Nickname: "test", Attempts: 1, Difficulty: "facile"},
+	}
+
+	for _, player := range LeaderboardList {
+		if player.Attempts > 0 {
+			LeaderboardList = append(LeaderboardList, player)
+		}
+	}
+
+	t, err := template.ParseGlob("./front-end/leaderboard.gohtml")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = t.Execute(w, LeaderboardList)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
 }
